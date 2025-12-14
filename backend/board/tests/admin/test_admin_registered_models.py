8ecee4ca55c6a569ca540c5563e614f6221f2383
pikanyan <113081175+pikanyan@users.noreply.github.com>
2025-12-15 08:35:50 +0900

    test(board): admin に Department model を登録したか

---
+++ backend/board/tests/admin/test_admin_registered_models.py
@@ -0,0 +1,28 @@
+# backend/board/tests/admin/test_admin_registered_models.py
+import pytest
+
+from django.contrib.admin import site
+
+from board import models
+
+
+
+@pytest.mark.parametrize\
+(
+    "model",
+    [
+        models.Department,
+    ],
+)
+def test_admin_registers_board_models(model):
+    # Arrange
+
+
+
+    # Act
+    is_registered = model in site._registry
+
+
+
+    # Assert
+    assert is_registered
