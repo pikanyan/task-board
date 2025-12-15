82675f4c67ba9b11ba6382decdaf2e16de2c6010
pikanyan <113081175+pikanyan@users.noreply.github.com>
2025-12-15 13:16:18 +0900

    t/0013 board department bootstrap (#13)

--- backend/board/tables.py
+++ backend/board/tables.py
@@ -7,6 +7,11 @@ from board.models import Department
 
 # 追加
 class DepartmentTable(tables.Table):
+    # name を詳細リンクにする
+    name = tables.LinkColumn("department_detail", args=[tables.A("pk")])
+
+
+    
     class Meta:
         model = Department
 
